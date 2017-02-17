08a0aa3f506f72471694631dbd08ab52e5b53e3d
Clifford Grimmell <agrimmell2@gmail.com>
2017-02-17 13:48:33 -0800

    add form for name so that name will appear in alert box when form is submitted

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(function () { //main function
   $("form#quiz").submit(function(event)  {
+    var userName = $("input#name").val();
     var question1 = $("#quiz input[name='question1']:checked").val();
     var question2 = $("#quiz input[name='question2']:checked").val();
     var question3 = $("#quiz input[name='question3']:checked").val();
@@ -10,14 +11,12 @@ $(function () { //main function
     var question8 = $("#quiz input[name='question8']:checked").val();
     var answers = [question1, question2, question3, question4, question5, question6, question7, question8];
 
-    function mode (array) { //unfortunately this part was really difficult, I had to try and find help on stackexchange, but the implementation works well.
-    if (array.length == 0)
+    function mode (array) { //unfortunately this part was really difficult, I had to try and find help on stackexchange to learn how to do this, but the implementation works well enough.
+    if (array.length == 0) //fortunately now I would be able to put as many test questions as I want and just have to se them up as variables. Much easier than writing if statements.
         return null;
-
     var modeMap = {},
         maxEl = array[0],
         maxCount = 1;
-
     for(var i = 0; i < array.length; i++){
         var el = array[i];
 
@@ -26,31 +25,32 @@ $(function () { //main function
         else
             modeMap[el]++;
 
-        if (modeMap[el] > maxCount)
-        {
+        if (modeMap[el] > maxCount) {
             maxEl = el;
             maxCount = modeMap[el];
         }
-        else if (modeMap[el] == maxCount)
-        {
+        else if (modeMap[el] == maxCount) {
             maxEl += '&' + el;
             maxCount = modeMap[el];
         }
     }
     return maxEl;
-    };
+  };
+    if (userName === ""){
+      alert("Please enter your name")
+    } else {
     var test = mode(answers);
     var test = parseInt(test);
     if (test === 1) {
-      alert("You should think about C#/.NET! However, the other courses are perfectly valid. Any one of them would be a great choice.")
+      alert(userName +", you should think about C#/.NET! However, the other courses are perfectly valid. Any one of them would be a great choice.")
     } else if (test === 2) {
-      alert("You should think about Ruby/Rails! However, the other courses are perfectly valid. Any one of them would be a great choice.")
+      alert(userName + ", you should think about Ruby/Rails! However, the other courses are perfectly valid. Any one of them would be a great choice.")
     } else if (test === 3) {
-      alert("You should think about Java/Android! However, the other courses are perfectly valid. Any one of them would be a great choice.")
+      alert(userName + ", you should think about Java/Android! However, the other courses are perfectly valid. Any one of them would be a great choice.")
     } else if (test === 4) {
-      alert("You should think about Ruby/Rails! However, the other courses are perfectly valid. Any one of them would be a great choice.")
+      alert(userName + ", you should think about Ruby/Rails! However, the other courses are perfectly valid. Any one of them would be a great choice.")
     } else {
-      alert("This isn't supposed to happen. Pls fix")
-    }
+      alert("Please fill out the form")
+    }};
    });
 });
