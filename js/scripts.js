4fbc4d9da4f08c27b2d7f9669c0572794c61d02c
Clifford Grimmell <agrimmell2@gmail.com>
2017-02-20 16:54:30 -0800

    fix code styling and structure and provide correct output of selection

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,23 @@
 $(function () {
   $("button#dark").click(function() {
-    $("body").removeClass();
-    $("body").addClass("dark-background");
-    $("#sidebar").removeClass();
-    $("#sidebar").addClass("sidebar");
-    $("#sidebar").addClass("dark-background2");
-    $("div#container1").removeClass();
-    $("div#container1").addClass("dark-background2");
-    $("div#container1").addClass("container");
+    $("body").removeClass().addClass("dark-background");
+    $("#sidebar").removeClass().addClass("sidebar dark-background2");
+    $("div#container1").removeClass().addClass("dark-background2 container");
     $("#after").addClass("after-text")
   });
   $("button#light").click(function() {
-    $("body").removeClass();
-    $("body").addClass("light-background");
-    $("#sidebar").removeClass();
-    $("#sidebar").addClass("sidebar");
-    $("#sidebar").addClass("light-background3");
-    $("div#container1").removeClass();
-    $("div#container1").addClass("light-background2");
-    $("div#container1").addClass("container");
+    $("body").removeClass().addClass("light-background");
+    $("#sidebar").removeClass().addClass("sidebar light-background3");
+    $("div#container1").removeClass().addClass("light-background2 container");
   }); //main function
   $("form#quiz").submit(function(event)  {
     var userName = $("input#name").val();
-    var question1 = $("#quiz input[name='question1']:checked").val();
-    var question2 = $("#quiz input[name='question2']:checked").val();
-    var question3 = $("#quiz input[name='question3']:checked").val();
-    var question4 = $("#quiz input[name='question4']:checked").val();
-    var question5 = $("#quiz input[name='question5']:checked").val();
-    var question6 = $("#quiz input[name='question6']:checked").val();
-    var question7 = $("#quiz input[name='question7']:checked").val();
-    var question8 = $("#quiz input[name='question8']:checked").val();
-    var answers = [question1, question2, question3, question4, question5, question6, question7, question8];
+    var answers = ["question1", "question2", "question3", "question4", "question5", "question6", "question7", "question8"];
+    var answers2 = [];
+    answers.forEach(function(question){
+      var questions = $("#quiz input[name='"+question+"']:checked").val();
+      answers2.push(questions);
+    });
 
     function mode (array) { //unfortunately this part was really difficult, I had to try and find help on stackexchange to learn how to do this, but the implementation works well enough.
     if (array.length == 0) //fortunately now I would be able to put as many test questions as I want and just have to se them up as variables. Much easier than writing if statements.
@@ -60,21 +47,21 @@ $(function () {
     if (userName === ""){
       alert("Please enter your name")
     } else {
-    var message = (", you should think about CSS/Design! However, the other courses are perfectly valid. Any one of them would be a great choice. If you would like to learn more, the resources on the sidebar are a great source. Refresh the page to take the quiz again!")
-    var test = mode(answers);
+    var message = (" However, the other courses are perfectly valid. Any one of them would be a great choice. If you would like to learn more, the resources on the sidebar are a great source. Refresh the page to take the quiz again!")
+    var test = mode(answers2);
     var test = parseInt(test);
     if (test === 1) {
-      document.getElementById("after").innerHTML = (userName + message);
+      document.getElementById("after").innerHTML = (userName +", you should think about C#/.NET!" + message);
       $("#after").show();
       window.scrollto(0,2000);
     } else if (test === 2) {
-      document.getElementById("after").innerHTML = (userName + message);
+      document.getElementById("after").innerHTML = (userName +", you should think about Ruby/Rails!" + message);
       $("#after").show();
     } else if (test === 3) {
-      document.getElementById("after").innerHTML = (userName + message);
+      document.getElementById("after").innerHTML = (userName +", you should think about Java/Android!" + message);
       $("#after").show();
     } else if (test === 4) {
-      document.getElementById("after").innerHTML = (userName + message);
+      document.getElementById("after").innerHTML = (userName +", you should think about CSS/Design~!" + message);
       $("#after").show();
     } else {
       alert("Please fill out the form")
